e32ed5b4154b0f71c308cd65d10251635e189a2f
Magui <magui@Maguis-MacBook-Pro.local>
2021-06-15 09:10:05 -0300

    button

--- cv.js
+++ cv.js
@@ -2,12 +2,11 @@ const sections = document.querySelectorAll(".section-js");
 
 
 function handleClick(e) {
-    console.log(e)
     let selected = e.target; 
-    selected.classList.add('sss');
     let lastChild = selected.lastElementChild;
-    lastChild.classList.add('opacity');
     let icon = lastChild.children[0];
+
+    selected.classList.add('sss');
     icon.onclick = function dd () {
         selected.classList.remove("sss");
     }
