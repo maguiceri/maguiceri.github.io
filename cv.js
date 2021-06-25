14fb379e2c08b379b48c38005f3b6d898fd25421
Magui <magui@Maguis-MacBook-Pro.local>
2021-06-25 19:20:16 -0300

    photo

--- cv.js
+++ cv.js
@@ -1,13 +1,16 @@
 const sections = document.querySelectorAll(".section-js");
 
-
 const handleClick = (section) => () => {
-    section.classList.toggle('sss');   
-}
+  const page = section.getElementsByClassName("page")[0];
+  if (page.style.maxHeight) {
+    page.style.padding = "0";
+    page.style.maxHeight = null;
+  } else {
+    page.style.padding = "10px 0";
+    page.style.maxHeight = page.scrollHeight + "px";
+  }
+};
 
-sections.forEach(section => { section.onclick = handleClick(section)})
-
-
-
-
-
+sections.forEach((section) => {
+  section.onclick = handleClick(section);
+});
