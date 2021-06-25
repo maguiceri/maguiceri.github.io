fce0fc778dd9974fcfc55f7d4d3b4b2210159f6f
Magui <magui@Maguis-MacBook-Pro.local>
2021-06-25 17:05:29 -0300

    transition

--- cv.js
+++ cv.js
@@ -1,21 +1,11 @@
 const sections = document.querySelectorAll(".section-js");
 
 
-function handleClick(e) {
-    let selected = e.target; 
-    let lastChild = selected.lastElementChild;
-    console.log(lastChild);
-    let icon = lastChild.children[0];
-
-    selected.classList.add('sss');
-    icon.onclick = function dd () {
-        selected.classList.remove("sss");
-    }
-    
-   
+const handleClick = (section) => () => {
+    section.classList.toggle('sss');   
 }
 
-sections.forEach(section => { section.onclick = handleClick})
+sections.forEach(section => { section.onclick = handleClick(section)})
 
 
 
